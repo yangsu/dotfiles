ab389c2fa1db291dc871caeec3f7dff7240ea875
Yang Su <suyang1@gmail.com>
2020-12-07 19:27:25 -0800

    Added .duti file

--- bootstrap.sh
+++ bootstrap.sh
@@ -9,6 +9,8 @@ function doIt() {
 		--exclude ".DS_Store" \
 		--exclude ".osx" \
 		--exclude "bootstrap.sh" \
+		--exclude "brew.sh" \
+		--exclude ".duti" \
 		--exclude "README.md" \
 		--exclude "LICENSE-MIT.txt" \
 		-avh --no-perms . ~;
